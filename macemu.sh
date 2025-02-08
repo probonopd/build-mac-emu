2ef91dae12393825edea0583f4883a0e4f5ddafd
probonopd <probonopd@users.noreply.github.com>
2025-02-08 18:44:27 +0100

    Create macemu.sh
    [ci skip]

---
+++ macemu.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e -x
+
+cat /etc/os-release # Debian GNU/Linux 12 (bookworm)
+cat /etc/rpi-issue # Raspberry Pi reference 2024-11-19
+
+sudo apt-get -y update
+sudo apt-get -y install git unzip libgmp-dev libmpfr-dev libsdl2-dev autoconf
+
+git clone https://github.com/kanjitalk755/macemu.git
+cd macemu
+git checkout 6ddff7b
+
+cd BasiliskII/src/Unix
+./autogen.sh --prefix=/usr
+make -j$(nproc)
+sudo make install
+sudo checkinstall --pkgname=basiliskii-custom --pkgversion=$(date +%Y%m%d) --backup=no --install=no --default
+
+cd SheepShaver/src/Unix
+./autogen.sh --prefix=/usr
+make -j$(nproc)
+sudo make install
+sudo checkinstall --pkgname=sheepshaver-custom --pkgversion=$(date +%Y%m%d) --backup=no --install=no --default
