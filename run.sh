0d64df0e7d736e0f74576d16b1c2850e044e0f49
probonopd <probonopd@users.noreply.github.com>
2025-02-08 23:31:28 +0100

    Update run.sh
    [ci skip]

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ mount | grep "on /boot/firmware" || exit 0
 
 cd ~
 wget "https://github.com/adamhope/rpi-basilisk2-sdl2-nox/raw/refs/heads/main/Quadra800.ROM" -O ROM
-wget "https://github.com/mihaip/infinite-mac/raw/refs/heads/main/Images/Mac%20OS%207.6%20HD.dsk" -O system.img
+wget "https://github.com/mihaip/infinite-mac/raw/refs/heads/main/Images/Mac%20OS%207.6%20HD.dsk" -O system.img # TODO: Find a cleaner one
 mkdir -p ~/.config/BasiliskII/
 echo "disk system.img" > ~/.config/BasiliskII/prefs
 echo "frameskip 0" >> ~/.config/BasiliskII/prefs
