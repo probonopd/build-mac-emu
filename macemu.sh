8cd2e10650fa74854919e88677ca97a9e640c687
probonopd <probonopd@users.noreply.github.com>
2025-02-08 23:24:59 +0100

    Update macemu.sh
    [ci skip]

--- macemu.sh
+++ macemu.sh
@@ -4,7 +4,7 @@ cat /etc/os-release # Debian GNU/Linux 12 (bookworm)
 cat /etc/rpi-issue # Raspberry Pi reference 2024-11-19
 
 sudo apt-get -y update
-sudo apt-get -y install git unzip libgmp-dev libmpfr-dev libsdl2-dev autoconf
+sudo apt-get -y install git unzip libgmp-dev libmpfr-dev libsdl2-dev autoconf patchelf
 
 git clone https://github.com/kanjitalk755/macemu.git
 cd macemu
@@ -24,8 +24,8 @@ cd -
 cd SheepShaver/src/Unix
 ./autogen.sh --prefix=/usr
 make -j$(nproc)
-# sudo make install
 make DESTDIR=app install
+strip app/usr/bin/*
 mkdir -p app/usr/lib
 ldd app/usr/bin/*  | grep "=>" | cut -d " " -f 3 | xargs -I {} cp {} app/usr/lib/
 find app/usr/bin/ -type f -exec patchelf --add-rpath '$ORIGIN/../lib' {} \;
