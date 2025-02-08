625200c1e9698f03ff0a0e661c51ae5f247581c6
probonopd <probonopd@users.noreply.github.com>
2025-02-08 22:54:19 +0100

    Update macemu.sh
    [ci skip]

--- macemu.sh
+++ macemu.sh
@@ -4,7 +4,7 @@ cat /etc/os-release # Debian GNU/Linux 12 (bookworm)
 cat /etc/rpi-issue # Raspberry Pi reference 2024-11-19
 
 sudo apt-get -y update
-sudo apt-get -y install git unzip libgmp-dev libmpfr-dev libsdl2-dev autoconf checkinstall
+sudo apt-get -y install git unzip libgmp-dev libmpfr-dev libsdl2-dev autoconf
 
 git clone https://github.com/kanjitalk755/macemu.git
 cd macemu
