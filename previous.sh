519f2a1f2ecf6bdc136c6b579236b87462d502a7
probonopd <probonopd@users.noreply.github.com>
2025-02-09 14:34:51 +0100

    Update previous.sh
    [ci skip]

--- previous.sh
+++ previous.sh
@@ -21,3 +21,12 @@ find app/usr/bin/ -type f -exec patchelf --add-rpath '$ORIGIN/../lib' {} \;
 ( cd app/ ; zip -r ../previous.zip * )
 mount | grep "on /boot/firmware" && sudo cp *.zip /boot/firmware/
 cd -
+
+# FIXME: Disable X11 and Wayland, how?
+
+# wget https://winworldpc.com/download/45e2809d-53c2-b753-3e11-c3a4c2a90f70/from/c39ac2af-c381-c2bf-1b25-11c3a4e284a2
+# sudo apt install 7zip
+# 7zr x c39ac2af-c381-c2bf-1b25-11c3a4e284a2
+# previous ./Nextstep\ 1.0\ HD\ Image\ With\ Previous/NS10_1GB.dd
+# Only a black screen with a cursor, why?
+# How to specify ROM and disk?
