a6001b8fe7e276644c302a52559c2230bed66d96
probonopd <probonopd@users.noreply.github.com>
2025-02-09 11:46:28 +0100

    Update minivmac.sh
    [ci skip]

--- minivmac.sh
+++ minivmac.sh
@@ -12,3 +12,7 @@ ldd app/usr/bin/*  | grep "=>" | cut -d " " -f 3 | xargs -I {} cp {} app/usr/lib
 find app/usr/bin/ -type f -exec patchelf --add-rpath '$ORIGIN/../lib' {} \;
 ( cd app/ ; zip -r ../minivmac.zip * )
 mount | grep "on /boot/firmware" && sudo cp *.zip /boot/firmware/
+
+# wget -c "https://github.com/archtaurus/RetroPieBIOS/raw/master/BIOS/MacII.ROM"
+# wget "https://github.com/mihaip/infinite-mac/raw/refs/heads/main/Images/System%206.0.8%20HD.dsk"
+# ./app/usr/bin/minivmac System\ 6.0.8\ HD.dsk
